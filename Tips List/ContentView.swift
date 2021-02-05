8cf310c842459979a56ce23a829e293ba4c493b7
张亚飞 <672233580@qq.com>
2021-02-05 23:21:56 +0800

    Initial commit

---
+++ Tips List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tips List
+//
+//  Created by 张亚飞 on 2021/2/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tips List/Tips_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tips_ListApp.swift
+//  Tips List
+//
+//  Created by 张亚飞 on 2021/2/5.
+//
+
+import SwiftUI
+
+@main
+struct Tips_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
